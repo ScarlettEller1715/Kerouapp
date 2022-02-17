86a2d79053ffb82538939e4653759aa6e860b6d8
Scarlett Eller <rq6715@gmail.com>
2022-02-17 16:27:05 -0500

    final itinerary CSS

--- src/RenderTrip.js
+++ src/RenderTrip.js
@@ -15,10 +15,12 @@ function RenderTrip({ origin, destination, distance, totalTime, timeString, gasC
             <h2 className="tripLabel">{origin} to {destination}</h2>
             <h4 className="tripTime">{timeString}</h4>
             <p className="tripDistance">{distance} miles</p>
-            <p>It will cost approximately ${gasCost} for the gas this trip requires.</p>
+            <p className="tripGas">It will cost approximately ${gasCost} for the gas this trip requires.</p>
             <div className="try">
             <button className="tripDeleteButton" onClick={handleClick}>X</button>
             </div>
+            <hr className="Line">
+            </hr>
         </div>
     )
 }
