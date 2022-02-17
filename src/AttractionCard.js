36e683177b9c8208b87917e17ddcb921475645f8
Dan Baechtold <latebloomertech@gmail.com>
2022-02-17 14:41:20 -0500

    Attraction cards and fuel economy

--- src/AttractionCard.js
+++ src/AttractionCard.js
@@ -5,12 +5,14 @@ function AttractionCard({attraction}) {
 
 
   return (
-    <div>
+
+    <div className="card">
         <h2>{attraction.name}</h2>
-        <p>{attraction.Website}</p>
+        <a href={attraction.Website}>Website</a>
         <p>{attraction.Address.formattedAddress}</p>
         <p>{attraction.PhoneNumber}</p>
     </div>
+
   )
 }
 
