a82d21441b8be83b7a9a4ef3ef7a46ced05a4932
Dan Baechtold <latebloomertech@gmail.com>
2022-02-18 11:55:07 -0500

    Fade in text

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+
+
 function Home() {
-    
+
 const navigate = useNavigate()
 
 function handleClick() {
     navigate('/planning')
 }
-    
+
     return(
         <div className="Homepage">
-            <h1 className="hpTitle">Welcome to KerouApp</h1>
-            <h2 className="hpSub">Your roadtrip planning application</h2>
-            <button className="enterButton" onClick={handleClick}>Start your adventure...</button>
+            <h1 className="fade-in-text">Welcome to KerouApp</h1>
+            <h2 className="fade-in-sub">Your roadtrip planning application</h2>
+            <button className="fade-in-button" onClick={handleClick}>The Adventure Starts Here...</button>
         </div>
     )
 }
